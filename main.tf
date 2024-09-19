a6e0b40746fe805dbec0f5436fa60fc606849fba
José Lisboa <josemrlisboa@gmail.com>
2024-09-19 19:07:30 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,19 +49,7 @@ module "db-mctech-sqlserver" {
   db_subnet_group_name   = module.vpc.database_subnet_group
   vpc_security_group_ids = [module.security_group.security_group_id]
 
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["error"]
-  create_cloudwatch_log_group     = true
-
-  backup_retention_period = 1
-  skip_final_snapshot     = true
   deletion_protection     = false
-
-  performance_insights_enabled          = false
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
 
   options                   = []
   create_db_parameter_group = false
