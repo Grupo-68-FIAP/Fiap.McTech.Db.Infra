2dd0fd09fc77f88bddd3192b3945ad97bce9a75a
Jose Lisboa <jose.lisboa@thstec.com.br>
2024-09-22 19:32:44 -0300

    Correções no main.tf de criação da BD em RDS

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Centralizando algumas variáveis locais nesta seção
 locals {
-  name    = "mctech-sqlserver"
+  name    = "mctech-sqlserverdb"
   region  = "us-east-1"
 
   tags = {
@@ -18,7 +18,7 @@ provider "aws" {
 resource "aws_default_vpc" "default_vpc" {
 
   tags = {
-    Name = "mctech-sqlserver-vpc"
+    Name = "mctech-sqlserverdb-vpc"
   }
 }
 
@@ -38,8 +38,8 @@ resource "aws_default_subnet" "subnet_az2" {
 }
 
 # Criação de security group para acesso ao banco pela API
-resource "aws_security_group" "webserver_security_group" {
-  name        = "webserver-security-group"
+resource "aws_security_group" "mctechws_security_group" {
+  name        = "mctechws-security-group"
   description = "Enable HTTP access on port 80"
   vpc_id      = aws_default_vpc.default_vpc.id
 
@@ -59,13 +59,13 @@ resource "aws_security_group" "webserver_security_group" {
   }
 
   tags   = {
-    Name = "mctech-sqlserver-web-security-group"
+    Name = "mctech-sqlserverdb-web-security-group"
   }
 }
 
 # Criação de security group para acesso ao banco de dados
-resource "aws_security_group" "database_security_group" {
-  name        = "database-security-group"
+resource "aws_security_group" "mctechdb_security_group" {
+  name        = "mctechdb-security-group"
   description = "enable SQLServer access on port 1433"
   vpc_id      = aws_default_vpc.default_vpc.id
 
@@ -74,46 +74,39 @@ resource "aws_security_group" "database_security_group" {
     from_port        = 1433
     to_port          = 1433
     protocol         = "tcp"
-    security_groups  = [aws_security_group.webserver_security_group.id]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags   = {
-    Name = "mctech-sqlserver-database-security-group"
+    Name = "mctech-sqlserverdb-database-security-group"
   }
 }
 
 
 # Cria grupo de subredes para a instância RDS
-resource "aws_db_subnet_group" "database_subnet_group" {
-  name         = "database-subnets"
+resource "aws_db_subnet_group" "mctechdb_subnet_group" {
+  name         = "mctechdb-subnets"
   subnet_ids   = [aws_default_subnet.subnet_az1.id, aws_default_subnet.subnet_az2.id]
   description  = "Subnets for database instance"
 
   tags   = {
-    Name = "mctech-sqlserver-database-subnets"
+    Name = "mctech-sqlserverdb-database-subnets"
   }
 }
 
 
 # Cria a instância RDS em si
-resource "aws_db_instance" "db_instance" {
+resource "aws_db_instance" "mctechdb_instance" {
   engine                  = "sqlserver-ex"
   engine_version          = "15.00"
   multi_az                = false
-  identifier              = "mctech-sqlserver-rds-instance"
+  identifier              = "mctech-sqlserverdb-rds-instance"
   username                = "mctech"
   password                = "changeit123" # Adicionar a secret
   instance_class          = "db.t3.micro"
-  allocated_storage       = 200
-  db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.database_security_group.id]
+  allocated_storage       = 50
+  db_subnet_group_name    = aws_db_subnet_group.mctechdb_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.mctechdb_security_group.id]
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
   skip_final_snapshot     = true
 }
